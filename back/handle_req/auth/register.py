7290172d324520c7447cdc5bdef4047beebf070c
bentorrrr <bentorrrrr@gmail.com>
2023-10-15 01:26:06 +0700

    Handle register request

--- back/handle_req/auth/register.py
+++ back/handle_req/auth/register.py
@@ -14,5 +14,23 @@ class RegisterRequestBody(BaseModel):
 
 class Register(HandleRequest[RegisterRequestBody, None]):
     async def _handle(self) -> Response:
-        self.logger.debug(self.body)
+        self.logger.debug(self.body)        
+        collection = self.mongo_client["GuHiw"]["User"]     
+        # Gen Salt + Hash Password
+        hashed_password = bcrypt.hashpw(self.body.password.encode("ascii"), bcrypt.gensalt())
+        # Hash Password
+        # Check if username or email already exists
+        if(collection.find_one({"email": self.body.email})):
+            self.logger.info("400 Error")
+            return Response("Email already exists", status_code=400)
+        if(collection.find_one({"username": self.body.username})):
+            self.logger.info("400 Error")
+            return Response("Username already exists", status_code=400)
+        hashedBody = self.body.model_dump()
+        hashedBody["password"] = hashed_password
+        try:            
+            collection.insert_one(hashedBody)
+        except Exception as e:
+            self.logger.error(e)
+            return Response("error", status_code=500)
         return Response("register")
