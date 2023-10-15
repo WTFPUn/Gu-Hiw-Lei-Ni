91c1ebb9a0570d19a3c7777bf5c913d259ea3f11
Pannapat Sittiopasanun <nut.pannapat@gmail.com>
2023-10-15 16:28:28 +0700

    fix cors

--- back/main.py
+++ back/main.py
@@ -4,6 +4,8 @@ import os
 
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 from uvicorn import Config, Server
 from logging import Logger
 from handle_req.auth import Auth
@@ -16,10 +18,13 @@ MONGO_URL = os.getenv("MONGO_URL")
 logging: Logger = Logger(__name__)
 logging.setLevel(0)
 
+middleware = [
+    Middleware(CORSMiddleware, allow_origins=['*'],allow_methods=['*'],allow_headers=['*'],allow_credentials=True)
+]
 
 async def main() -> None:
     # init app
-    app = Starlette(routes=[])
+    app = Starlette(routes=[],middleware=middleware)
 
     # init mongo client
     mongo_client: MongoClient = MongoClient(MONGO_URL)
@@ -28,7 +33,7 @@ async def main() -> None:
     auth = Auth(mongo_client)
     Auth.load_mongo_client(auth, mongo_client)
     app.routes.extend(auth.route())
-
+    app.add_middleware(middleware)
     # init server
     server: Server = Server(
         Config(
