d5985f84b85b5b7636876f927ebea9c713f2c46f
WTFPUn <punn_namw@hotmail.com>
2023-11-03 22:52:33 +0700

    implement pubsub system

--- back/pub_sub.py
+++ back/pub_sub.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
-from typing import List, Set, Dict, Callable, Any, TypeVar, Generic, Coroutine
+from typing import List, Set, Dict, Callable, Any, TypeVar, Generic, Coroutine, Tuple
 from logging import Logger
 import logging
+from handle_ws.client import Client
 
 from starlette.websockets import WebSocket
 
@@ -17,27 +18,56 @@ logging.basicConfig(level=logging.NOTSET)
 # set logger format
 logger: Logger = Logger(__name__)
 
+Channel = Tuple[str, ...]
+
+
+class PubSubChannelError(Exception):
+    pass
+
 
 class PubSub:
+    def __init__(self) -> None:
+        self.subscribers: Dict[Channel, Set[Client]] = {}
+        self.channel_message: Dict[Channel, BaseModel] = {}
+
     # TODO: add main method
-    def register(self, channel: Set[str]):
-        pass
+    def register(self, channel: Channel, message: BaseModel):
+        if channel not in self.subscribers:
+            self.subscribers[channel] = set()
+            self.channel_message[channel] = message
+        else:
+            raise PubSubChannelError("Channel already exists")
 
-    def unregister(self, channel: Set[str]):
-        pass
+    def unregister(self, channel: Channel):
+        if channel in self.subscribers:
+            del self.subscribers[channel]
+            del self.channel_message[channel]
+        else:
+            raise PubSubChannelError("Channel does not exist")
 
-    def publish(self, channel: str, message: Any):
-        pass
+    async def publish(self, channel: Channel, message: Any):
+        if channel in self.subscribers:
+            self.channel_message[channel] = message
+            for subscriber in self.subscribers[channel]:
+                await subscriber.callback.send_json(
+                    self.channel_message[channel].model_dump()
+                )
+        else:
+            raise PubSubChannelError("Channel does not exist")
 
-    def subscribe(
-        self, channel: str, callback: Callable[[str, Any], Coroutine[Any, Any, None]]
-    ):
-        pass
+    def subscribe(self, channel: Channel, client: Client):
+        if channel in self.subscribers:
+            self.subscribers[channel].add(client)
+            client.add_service(self, channel)
+        else:
+            raise PubSubChannelError("Channel does not exist")
 
-    def unsubscribe(
-        self, channel: str, callback: Callable[[str, Any], Coroutine[Any, Any, None]]
-    ):
-        pass
+    def unsubscribe(self, channel: Channel, client: Client):
+        if channel in self.subscribers:
+            self.subscribers[channel].remove(client)
+            client.remove_service(self, channel)
+        else:
+            raise PubSubChannelError("Channel does not exist")
 
-    def get(self):
-        pass
+    # def get(self):
+    #     pass
