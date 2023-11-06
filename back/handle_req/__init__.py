952fb7b38bc30580fe812dd240683d8088f1a9d0
WTFPUn <punn_namw@hotmail.com>
2023-11-06 22:24:18 +0700

    format file

--- back/handle_req/__init__.py
+++ back/handle_req/__init__.py
@@ -24,12 +24,12 @@ class HandleRequest(Generic[GenericRequestBody, GenericRequestQParam], ABC):
     def __init__(
         self,
         method: List[methods],
-        RequestBody: BaseModel = None,
-        RequestQParam: BaseModel = None,
+        RequestBody: BaseModel | None = None,
+        RequestQParam: BaseModel | None = None,
     ) -> None:
         self.logger: Logger = logger.getChild(self.__class__.__name__)
         self.method = method
-        self.mongo_client: MongoClient = None
+        self.mongo_client: MongoClient
         self.RequestBody = RequestBody
         self.RequesstQParam = RequestQParam
 
@@ -44,11 +44,11 @@ class HandleRequest(Generic[GenericRequestBody, GenericRequestQParam], ABC):
                 if request.method in ["POST", "PUT"]:
                     body = await request.json()
                     self.logger.debug(body)
-                    self.body = self.RequestBody.model_validate(body) # type: ignore
+                    self.body = self.RequestBody.model_validate(body)  # type: ignore
 
             if self.RequesstQParam != None:
                 if "GET" == request.method:
-                    self.query = self.RequesstQParam.model_validate(request.query_params) # type: ignore
+                    self.query = self.RequesstQParam.model_validate(request.query_params)  # type: ignore
 
             return await self._handle()
         except Exception as e:
@@ -56,7 +56,9 @@ class HandleRequest(Generic[GenericRequestBody, GenericRequestQParam], ABC):
             return Response("Bad Request", status_code=400)
 
     @abstractmethod
-    async def _handle(self) -> Response:
+    async def _handle(
+        self,
+    ) -> Response:
         pass
 
     def load_mongo_client(self, mongo_client: MongoClient) -> None:
