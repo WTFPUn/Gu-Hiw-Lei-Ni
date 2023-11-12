843e11054b3932295aacd096f1ad5674a775b1a3
WTFPUn <punn_namw@hotmail.com>
2023-11-12 22:03:42 +0700

    update handling attribute to static attribute

--- back/handle_req/endpoint_collector.py
+++ back/handle_req/endpoint_collector.py
@@ -4,16 +4,17 @@ from handle_req import HandleRequest
 
 
 class EndpointCollector:
+    handlings: List[HandleRequest] = []
+
     def __init__(self, mongo_client) -> None:
         self.mongo_client = mongo_client
-        self.handlings: List[HandleRequest] = []
 
     def __str__(self) -> str:
         return "/" + self.__class__.__name__.lower()
 
     def route(self) -> List[Route]:
         return [
-            Route(self.__str__() + str(h), h.handle, methods=h.method)
+            Route(self.__str__() + str(h), h.handle, methods=list(h.method))
             for h in self.handlings
         ]
 
