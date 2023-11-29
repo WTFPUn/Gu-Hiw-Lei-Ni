80f2d3cd3347d8746f2b17b0340e4eae7d6936b3
WTFPUn <punn_namw@hotmail.com>
2023-11-29 22:54:25 +0700

    fix editprofile req field access

--- back/handle_req/profile/editprofile.py
+++ back/handle_req/profile/editprofile.py
@@ -6,35 +6,46 @@ from pydantic import BaseModel
 from starlette.responses import Response, JSONResponse
 from handle_req import HandleRequest
 from typing import Optional
+from type.User import User
+
+
 class EditProfileRequest(BaseModel):
-    user_id: str
     first_name: Optional[str] = None
     last_name: Optional[str] = None
     # profile_picture: bytes
-    confirm_password: Optional[str] = None
+    confirm_password: str
 
 
 class EditProfileBody(BaseModel):
     data: EditProfileRequest
     token: str
 
+
 class EditProfile(HandleRequest[EditProfileBody, None]):
-    async def _handle(self) -> Response:        
+    async def _handle(self) -> Response:
         if self.mongo_client is None:
             raise Exception("Mongo client is not set")
-        req = self.body.data.model_dump(exclude_none=True)
+        req = self.body.data.model_dump()
         user_data = jwt.decode(
             self.body.token, os.getenv("JWT_SECRET"), algorithms=["HS256"]
         )
         collection = self.mongo_client["GuHiw"]["User"]
-        user = user_data.user_id.find_one({"user_id": req.user_id})
-        if user:
-            if bcrypt.checkpw(req.confirm_password.encode("ascii"), user["password"]):
-                if req.first_name:
-                    user["first_name"] = req.first_name
-                if req.last_name:
-                    user["last_name"] = req.last_name
-                collection.update_one({"user_id": req.user_id}, {"$set": user})
+        user = collection.find_one({"user_id": user_data["user_id"]})
+
+        user_val = User.model_validate(user)
+
+        if user and user_val.password:
+            if bcrypt.checkpw(
+                req["confirm_password"].encode("ascii"), user_val.password
+            ):
+                if req["first_name"]:
+                    user_val.first_name = req["first_name"]
+                if req["last_name"]:
+                    user_val.last_name = req["last_name"]
+
+                collection.update_one(
+                    {"user_id": user_val.user_id}, {"$set": user_val.model_dump()}
+                )
                 return JSONResponse({"status": True})
             else:
                 return Response("Password is incorrect", status_code=401)
